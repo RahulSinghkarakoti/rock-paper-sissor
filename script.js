1d3f8d84f78f03579d2fc98db90f4d0a43e8ed3c
rahulsingh <117975372+rahulkarakoti@users.noreply.github.com>
2024-03-09 14:16:26 +0530

    modified:   index.html 	modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
 let userscore = 0;
 let compscore = 0;
 
-
 let choices = document.querySelectorAll(".choice");
-var msg=document.querySelector(".msg .box h2");
-var score = document.querySelectorAll('.score h3');
-var msg_box=document.querySelector(".box");
+var msg = document.querySelector(".msg .box h2");
+var score = document.querySelectorAll(".score h3");
+var msg_box = document.querySelector(".box");
+var reset = document.querySelector(".box-reset");
 
+ 
+  reset.addEventListener("click", () => {
+    userscore = 0;
+    compscore = 0;
+    score[0].innerHTML = `${userscore}`;
+    score[1].innerHTML = `${compscore}`;
+    console.log("rest pressed");
+  });
+ 
 
-
-const comp=()=>{
-    const options=['rock','paper','scissors'];
-    let idx=Math.floor(Math.random() * 3);
-    return options[idx];
-}
+const comp = () => {
+  const options = ["rock", "paper", "scissors"];
+  let idx = Math.floor(Math.random() * 3);
+  return options[idx];
+};
 
 const playgame = (userchoice) => {
-    let compchoice = comp();
-    console.log(compchoice);
-    if (compchoice == "rock" && userchoice == "paper") {
-        msg.innerHTML = "You win!";
-        userscore++;
-        score[0].innerHTML = `${userscore}`; 
-        msg_box.style.backgroundColor="green";
-    } else if (compchoice == "scissors" && userchoice == "paper") {
-        msg.innerHTML = "You lose!";
-        userscore++;
-        score[0].innerHTML = `${userscore}`; 
-        msg_box.style.backgroundColor="red";
-
-    } else if (compchoice == "rock" && userchoice == "scissors") {
-        msg.innerHTML = "You lose!";
-        compscore++;
-        score[1].innerHTML = `${compscore}`; 
-        msg_box.style.backgroundColor="red";
-
-    } else if (compchoice == "paper" && userchoice == "scissors") {
-        msg.innerHTML = "You win!";
-        userscore++;
-        score[0].innerHTML = `${userscore}`; 
-        msg_box.style.backgroundColor="green";
-
-    } else if (compchoice == "paper" && userchoice == "rock") {
-        msg.innerHTML = "You lose!";
-        compscore++;
-        score[1].innerHTML = `${compscore}`; 
-        msg_box.style.backgroundColor="red";
-
-    } else if (compchoice == "scissors" && userchoice == "rock") {
-        msg.innerHTML = "You win!";
-        userscore++;
-        score[1].innerHTML = `${compscore}`; 
-        msg_box.style.backgroundColor="green";
-
-    } else {
-        msg.innerHTML = "Draw";
-        msg_box.style.backgroundColor="#DBE2EF";
-
-    }
-}
-
-
+  let compchoice = comp();
+  console.log(compchoice);
+  if (compchoice == "rock" && userchoice == "paper") {
+    msg.innerHTML = "You win!";
+    userscore++;
+    score[0].innerHTML = `${userscore}`;
+    msg_box.style.backgroundColor = "green";
+  } else if (compchoice == "scissors" && userchoice == "paper") {
+    msg.innerHTML = "You lose!";
+    compscore++;
+    score[1].innerHTML = `${compscore}`;
+    msg_box.style.backgroundColor = "red";
+  } else if (compchoice == "rock" && userchoice == "scissors") {
+    msg.innerHTML = "You lose!";
+    compscore++;
+    score[1].innerHTML = `${compscore}`;
+    msg_box.style.backgroundColor = "red";
+  } else if (compchoice == "paper" && userchoice == "scissors") {
+    msg.innerHTML = "You win!";
+    userscore++;
+    score[0].innerHTML = `${userscore}`;
+    msg_box.style.backgroundColor = "green";
+  } else if (compchoice == "paper" && userchoice == "rock") {
+    msg.innerHTML = "You lose!";
+    compscore++;
+    score[1].innerHTML = `${compscore}`;
+    msg_box.style.backgroundColor = "red";
+  } else if (compchoice == "scissors" && userchoice == "rock") {
+    msg.innerHTML = "You win!";
+    userscore++;
+    score[0].innerHTML = `${userscore}`;
+    msg_box.style.backgroundColor = "green";
+  } else {
+    msg.innerHTML = "Draw";
+    msg_box.style.backgroundColor = "#DBE2EF";
+  }
+};
 
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
