d28d98ecf86703c10284cb8cbd788de2e4807304
rahulsingh <117975372+rahulkarakoti@users.noreply.github.com>
2024-03-09 13:51:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+let userscore = 0;
+let compscore = 0;
+
+
+let choices = document.querySelectorAll(".choice");
+var msg=document.querySelector(".msg .box h2");
+var score = document.querySelectorAll('.score h3');
+var msg_box=document.querySelector(".box");
+
+
+
+const comp=()=>{
+    const options=['rock','paper','scissors'];
+    let idx=Math.floor(Math.random() * 3);
+    return options[idx];
+}
+
+const playgame = (userchoice) => {
+    let compchoice = comp();
+    console.log(compchoice);
+    if (compchoice == "rock" && userchoice == "paper") {
+        msg.innerHTML = "You win!";
+        userscore++;
+        score[0].innerHTML = `${userscore}`; 
+        msg_box.style.backgroundColor="green";
+    } else if (compchoice == "scissors" && userchoice == "paper") {
+        msg.innerHTML = "You lose!";
+        userscore++;
+        score[0].innerHTML = `${userscore}`; 
+        msg_box.style.backgroundColor="red";
+
+    } else if (compchoice == "rock" && userchoice == "scissors") {
+        msg.innerHTML = "You lose!";
+        compscore++;
+        score[1].innerHTML = `${compscore}`; 
+        msg_box.style.backgroundColor="red";
+
+    } else if (compchoice == "paper" && userchoice == "scissors") {
+        msg.innerHTML = "You win!";
+        userscore++;
+        score[0].innerHTML = `${userscore}`; 
+        msg_box.style.backgroundColor="green";
+
+    } else if (compchoice == "paper" && userchoice == "rock") {
+        msg.innerHTML = "You lose!";
+        compscore++;
+        score[1].innerHTML = `${compscore}`; 
+        msg_box.style.backgroundColor="red";
+
+    } else if (compchoice == "scissors" && userchoice == "rock") {
+        msg.innerHTML = "You win!";
+        compscore++;
+        score[1].innerHTML = `${compscore}`; 
+        msg_box.style.backgroundColor="green";
+
+    } else {
+        msg.innerHTML = "Draw";
+        msg_box.style.backgroundColor="#DBE2EF";
+
+    }
+}
+
+
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userchoice = choice.getAttribute("id");
+    playgame(userchoice);
+    console.log(choice);
+  });
+});
